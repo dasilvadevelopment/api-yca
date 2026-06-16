93c66021999d6ea918f46c369aab34fb013964d4
Kevin Nunes da Silva <kevin.nunes.da.silva@nioz.nl>
2026-06-16 11:56:31 +0200

    last additions to card details

--- yugioh/models.py
+++ yugioh/models.py
@@ -13,11 +13,19 @@ class MonsterType(models.Model):
 class MonsterAttribute(models.Model):
     name = models.CharField(max_length=20)
 
+class FrameType(models.Model):
+    name = models.CharField(max_length=20)
+
+class Series(models.Model):
+    name = models.TextField()
+
 class CardDetails(CardInfo):
     rarity = models.ForeignKey('Rarity', related_name='Rarity', on_delete=models.CASCADE, null=True, blank=True)
     card_type = models.ForeignKey('CardType', related_name='CardType', on_delete=models.CASCADE, null=True, blank=True)
     monster_type = models.ForeignKey('MonsterType', related_name='MonsterType', on_delete=models.CASCADE, null=True, blank=True)
     monster_attribute = models.ForeignKey('MonsterAttribute', related_name='MonsterAttribute', on_delete=models.CASCADE, null=True, blank=True)
-    stars = models.IntegerField()
+    frame_type = models.ForeignKey('FrameType', related_name='FrameType', on_delete=models.CASCADE, null=True, blank=True)
+    full_series = models.ForeignKey('Series', related_name='Series', on_delete=models.CASCADE)
+    level = models.IntegerField(null=True, blank=True)
     attack = models.IntegerField()
     defense = models.IntegerField()
